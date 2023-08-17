c76292365fd5c037a10e97bb6e03360c0fc6115c
Ibrahim Abu Wasel <i.abuwasel@gmail.com>
2023-08-17 20:30:45 +0300

    few changes

--- selenium_functions.py
+++ selenium_functions.py
@@ -3,10 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()  # create a webdriver window and open it
-driver.get('https://demo.applitools.com/')  # open this url / site
+def init_driver(url):
+    driver = webdriver.Chrome()
+    driver.get(url)
+    return driver
 
-selectors = {'username':'#username', 'password':'#password', 'login':'#log-in'}
 def handle_element(driver, selector, value=0):
     element = driver.find_element(By.CSS_SELECTOR, selector)
     if value:
@@ -14,27 +15,37 @@ def handle_element(driver, selector, value=0):
     else:
         element.click()
 
+def get_items_as_number(driver, selector):
+    element = driver.find_element(By.CSS_SELECTOR, selector).text
+    return float(element.replace("$", ""))
+
+def check_balance(total_balance, due_today=0):
+    # Check if the balance is plus or minus
+    new_total_balance = total_balance - due_today
+    if new_total_balance >= 0:
+        print(f'Your Current Balance is in plus: {new_total_balance}')
+    else:
+        print(f'Your Current Balance is in minus: {new_total_balance}')
+
+url = 'https://demo.applitools.com/'
+selectors = {'username':'#username',
+             'password':'#password',
+             'login':'#log-in',
+             'total_balance':'body > div > div.layout-w > div.content-w > div > div > div.element-wrapper.compact.pt-4 > div.element-box-tp > div > div > div > div.balance.hidden-mobile > div.balance-value > span:nth-child(1)',
+             'due_today':'body > div > div.layout-w > div.content-w > div > div > div.element-wrapper.compact.pt-4 > div.element-box-tp > div > div > div > div:nth-child(3) > div.balance-value.danger',
+             }
+
+driver = init_driver(url)
+
 handle_element(driver, selectors['username'], 'ibrahim')
 handle_element(driver, selectors['password'], '123456')
 handle_element(driver, selectors['login'])
 
 time.sleep(2)
 
+total_balance = get_items_as_number(driver, selectors['total_balance'])
+due_today = get_items_as_number(driver, selectors['due_today'])
 
-total_balance = driver.find_element(By.CSS_SELECTOR, 'body > div > div.layout-w > div.content-w > div > div > div.element-wrapper.compact.pt-4 > div.element-box-tp > div > div > div > div.balance.hidden-mobile > div.balance-value > span:nth-child(1)').text
-due_today = driver.find_element(By.CSS_SELECTOR, 'body > div > div.layout-w > div.content-w > div > div > div.element-wrapper.compact.pt-4 > div.element-box-tp > div > div > div > div:nth-child(3) > div.balance-value.danger').text
-
-#Remove the $ symbol and convert the value from str to int
-new_total_balance = int(total_balance.replace("$", "")) - int(due_today.replace("$", ""))
-
-
-
-
-# Check if the balance is plus or minus
-if new_total_balance >= 0:
-    print(f'Your Current Balance is in plus: {new_total_balance}')
-else:
-    print(f'Your Current Balance is in minus: {new_total_balance}')
-
+check_balance(total_balance, due_today)
 
 time.sleep(2)
