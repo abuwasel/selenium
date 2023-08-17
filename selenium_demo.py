163022bbbb293d5e179bbe62fb7e65e442c616d5
Ibrahim Abu Wasel <i.abuwasel@gmail.com>
2023-08-17 20:50:08 +0300

    few changes

--- selenium_demo.py
+++ selenium_demo.py
@@ -1,14 +1,13 @@
 from selenium_functions import *
 
 url = 'https://demo.applitools.com/'
+driver = init_driver(url)
 selectors = {'username':'#username',
              'password':'#password',
              'login':'#log-in',
              'total_balance':'body > div > div.layout-w > div.content-w > div > div > div.element-wrapper.compact.pt-4 > div.element-box-tp > div > div > div > div.balance.hidden-mobile > div.balance-value > span:nth-child(1)',
              'due_today':'body > div > div.layout-w > div.content-w > div > div > div.element-wrapper.compact.pt-4 > div.element-box-tp > div > div > div > div:nth-child(3) > div.balance-value.danger',
              }
-
-driver = init_driver(url)
 
 handle_element(driver, selectors['username'], 'ibrahim')
 handle_element(driver, selectors['password'], '123456')
